ab50e97c691c13ee3a46245a451e10d66d7d3dd6
Phil Reynolds <github@purpleriver.dev>
2021-06-10 16:56:01 +0100

    add theme toggle and update styling

---
+++ assets/js/themeToggle.js
@@ -0,0 +1,59 @@
+
+const btn = document.querySelector("#theme-toggle");
+const label = btn.querySelector('#theme-label')
+// check to see if OS preferences for light or dark mode
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const prefersLightScheme = window.matchMedia("(prefers-color-scheme: light)");
+
+// check to see if local storage has a theme preference
+let currentTheme = localStorage.getItem("theme");
+
+function setTheme() {
+    //if no local storage check against system preferences
+    if (currentTheme === null) {
+        if (prefersDarkScheme.matches) {
+            currentTheme = "dark"
+        } else if (prefersLightScheme.matches) {
+            currentTheme = "light"
+        } else {
+            // if no preferences, default to multi theme
+            currentTheme = "multi"
+        }
+        setTheme()
+    } else if (currentTheme === "dark") {
+        document.body.classList.remove("light-mode");
+        document.body.classList.remove("multi-mode");
+        document.body.classList.add("dark-mode");
+        label.innerHTML = "Change to Light Mode";
+    } else if (currentTheme === "multi") {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("multi-mode");
+        label.innerHTML = "Change to Dark Mode";
+    } else if (currentTheme === "light") {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.remove("multi-mode");
+        document.body.classList.add("light-mode");
+        label.innerHTML = "Change to Colourful";
+    }
+}
+
+btn.addEventListener("click", function () {
+    if (currentTheme === "multi") {
+        currentTheme = "dark"
+        setTheme()
+    } else if (currentTheme === "dark") {
+        currentTheme = "light"
+        setTheme()
+    } else {
+        currentTheme = "multi";
+        setTheme()
+    }
+    localStorage.setItem("theme", currentTheme);
+});
+
+
+btn.addEventListener('mouseenter', () => label.classList.add('show-label'))
+btn.addEventListener('mouseleave', () => label.classList.remove('show-label'))
+
+setTheme()

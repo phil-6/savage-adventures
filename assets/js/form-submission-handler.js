bb07f12b4fa9f7c07fce4d763ea9b8db7ad6e05d
phil-6 <github@purpleriver.dev>
2022-03-17 15:39:29 +0000

    change message areas to text areas and remove js disabling enter

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -9,16 +9,6 @@
         }
     }
 
-    // disable submit on enter
-    window.addEventListener('keydown', function (e) {
-        if (e.keyIdentifier === 'U+000A' || e.keyIdentifier === 'Enter' || e.keyCode === 13) {
-            if (e.target.nodeName === 'INPUT' && e.target.type === 'text') {
-                e.preventDefault();
-                return false;
-            }
-        }
-    }, true);
-
     // get all data in form and return object
     function getFormData(form) {
         let elements = form.elements;
@@ -117,6 +107,7 @@
             forms[i].addEventListener("submit", handleFormSubmit, false);
         }
     }
+
     document.addEventListener("DOMContentLoaded", loaded, false);
 
     function disableAllButtons(form) {
